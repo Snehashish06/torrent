df34157ed983b5449de36bc2ea9751e6dec7ba43
adarshUC <adarshuc595@gmail.com>
2023-10-09 22:02:30 +0530

    Update update.py

--- update.py
+++ update.py
@@ -72,7 +72,7 @@ if UPSTREAM_REPO is not None:
                      && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
     repo = UPSTREAM_REPO.split('/')
-    UPSTREAM_REPO = f"https://github.com/{repo[-2]}/{repo[-1]}"
+    UPSTREAM_REPO = f"https://github.com/adarshuc/torrent}"
     if update.returncode == 0:
         log_info('Successfully updated with latest commits !!')
         log_info(f'UPSTREAM_REPO: {UPSTREAM_REPO} | UPSTREAM_BRANCH: {UPSTREAM_BRANCH}')
